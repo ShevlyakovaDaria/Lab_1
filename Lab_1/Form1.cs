79c0a8f2c5bf36562dd1da4fd9f51c5fb42024f4
ShevlyakovaDaria <darya_shevlyakov@mail.ru>
2016-10-29 13:56:48 +0500

    Second_step

--- Lab_1/Form1.cs
+++ Lab_1/Form1.cs
@@ -21,5 +21,10 @@ namespace Lab_1
         {
             label1.Text = "Нажата кнопка Button 1";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Нажата кнопка Button 2";
+        }
     }
 }
